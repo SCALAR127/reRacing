67db85e0da64ca4328d8ac816213c9a8aa580326
SCALAR127 <scalar127@gmail.com>
2024-04-09 12:28:38 +0900

    first

---
+++ test_p59.java
@@ -0,0 +1,13 @@
+
+
+class Main {
+    public static void main(String[] args) {
+        String input = System.console().readLine("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        String [] carNames = input.split("\\s*, \\s*");
+
+
+
+
+
+    }
+}
